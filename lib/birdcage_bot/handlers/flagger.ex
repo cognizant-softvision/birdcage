dd6b797b7503107e21de555fb42a75224854d3e9
Forest Carlisle <forest@hey.com>
2020-08-13 14:45:43 -0700

    fix: refactor the flagger slack message handler

--- lib/birdcage_bot/handlers/flagger.ex
+++ lib/birdcage_bot/handlers/flagger.ex
@@ -17,28 +17,26 @@ defmodule BirdcageBot.Handlers.Flagger do
   def handle_event(
         %Message{
           app: %{name: "Flagger"},
-          attachments: [%{author_name: deployment_name, text: "Canary is waiting for approval."}]
+          attachments: [%{author_name: deployment_name, text: text}]
         } = event,
-        %{app_url: app_url} = state
+        state
       ) do
-    Bot.say(event, "Click <#{app_url}|here> to approve _rollout_ of *#{deployment_name}*.")
+    app_url = Keyword.fetch!(state, :app_url)
+
+    reply_to(event, deployment_name, app_url, text)
 
     state
   end
 
-  def handle_event(
-        %Message{
-          app: %{name: "Flagger"},
-          attachments: [
-            %{author_name: deployment_name, text: "Canary promotion is waiting for approval."}
-          ]
-        } = event,
-        %{app_url: app_url} = state
-      ) do
-    Bot.say(event, "Click <#{app_url}|here> to approve _promotion_ of *#{deployment_name}*.")
+  def handle_event(_event, state), do: state
 
-    state
+  defp reply_to(event, name, url, "Canary is waiting for approval.") do
+    Bot.say(event, "Click <#{url}|here> to approve _rollout_ of *#{name}*.")
   end
 
-  def handle_event(_, state), do: state
+  defp reply_to(event, name, url, "Canary promotion is waiting for approval.") do
+    Bot.say(event, "Click <#{url}|here> to approve _promotion_ of *#{name}*.")
+  end
+
+  defp reply_to(_event, _name, _url, _), do: nil
 end
