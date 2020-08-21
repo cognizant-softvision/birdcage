d99af63f64eea1b57ba88435986440f91e255294
Forest Carlisle <forest@hey.com>
2020-08-21 09:35:02 -0700

    fix: update event styling and layout

--- lib/birdcage_web/live/event_component.ex
+++ lib/birdcage_web/live/event_component.ex
@@ -11,12 +11,21 @@ defmodule EventComponent do
     event.metadata.eventMessage
   end
 
-  def phase_class_bg("Initialized"), do: "bg-yellow-400"
-  def phase_class_bg("Waiting"), do: "bg-orange-400"
-  def phase_class_bg("Progressing"), do: "bg-green-400"
-  def phase_class_bg("Promoting"), do: "bg-teal-400"
-  def phase_class_bg("Finalising"), do: "bg-indigo-400"
-  def phase_class_bg("Succeeded"), do: "bg-purple-400"
-  def phase_class_bg("Failed"), do: "bg-red-400"
-  def phase_class_bg(_phase), do: "bg-gray-200"
+  # https://github.com/weaveworks/flagger/blob/8a5a0538fd5247f52bf404950c48ecde5f7177e4/pkg/apis/flagger/v1beta1/status.go#L38
+  def phase_class_bg(%{phase: "Initializing"}), do: "bg-purple-400"
+  def phase_class_bg(%{phase: "Initialized"}), do: "bg-purple-400"
+  def phase_class_bg(%{phase: "Waiting"}), do: "bg-orange-400"
+  def phase_class_bg(%{phase: "Progressing"}), do: "bg-yellow-400"
+  def phase_class_bg(%{phase: "Promoting"}), do: "bg-teal-400"
+  def phase_class_bg(%{phase: "Finalising"}), do: "bg-indigo-400"
+  def phase_class_bg(%{phase: "Succeeded"}), do: "bg-green-400"
+  def phase_class_bg(%{phase: "Failed"}), do: "bg-red-400"
+  def phase_class_bg(%{phase: "Terminating"}), do: "bg-red-400"
+  def phase_class_bg(%{phase: "Terminated"}), do: "bg-red-400"
+  def phase_class_bg(_event), do: "bg-gray-200"
+
+  def type_class_bg(%{metadata: %{eventType: "Normal"}}), do: "bg-white"
+  def type_class_bg(%{metadata: %{eventType: "Warning"}}), do: "bg-red-200"
+  def type_class_bg(%{metadata: %{eventType: "Error"}}), do: "bg-red-400"
+  def type_class_bg(_event), do: "bg-white"
 end
